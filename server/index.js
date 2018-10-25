d485cb29ce5cd08648c16f3373d2435860d20006
andesome <ling95613@163.com>
2018-10-25 23:44:38 +0800

    定义代码规范

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-const Koa = require("koa");
-const koaBody = require("koa-body");
-const Router = require("koa-router");
-const { PORT } = require("./config/config");
-const registerRoute = require("./routes/register");
+const Koa = require('koa');
+const koaBody = require('koa-body');
+const Router = require('koa-router');
+const { PORT } = require('./config/config');
+const registerRoute = require('./routes/register');
 
 const app = new Koa();
 const router = new Router();
@@ -11,14 +11,14 @@ app.use(async (ctx, next) => {
   const start = new Date();
   await next();
   const ms = new Date() - start;
-  ctx.set("X-Response-Time", `${ms}ms`);
-  ctx.set("Access-Control-Allow-Origin", "*");
-  ctx.set("Access-Control-Allow-Headers", "accept, accept-encoding, authorization, content-type, dnt, origin, user-agent, x-csrftoken, x-requested-with");
-  ctx.set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+  ctx.set('X-Response-Time', `${ms}ms`);
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Headers', 'accept, accept-encoding, authorization, content-type, dnt, origin, user-agent, x-csrftoken, x-requested-with');
+  ctx.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
 });
 
-router.get("/", ctx => {
-  ctx.body = "haaaaaaaaaa";
+router.get('/', (ctx) => {
+  ctx.body = 'haaaaaaaaaa';
 });
 
 app
